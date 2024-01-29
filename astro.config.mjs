93e09c928a670899678dfacd0983d9cf9a59bdb1
Miles Conn <milesjconn@gmail.com>
2024-01-29 16:53:26 -0500

    Update url assets config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  output: "static",
+  integrations: [tailwind(), mdx()],
+  // TODO: specify project
+  site: "https://www.andrew.cmu.edu/course/16-726-sp24/projects/mconn/proj0/",
+  build: {
+    assetsPrefix:
+      "https://www.andrew.cmu.edu/course/16-726-sp24/projects/mconn/proj0/",
+  },
 });
