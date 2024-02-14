2d631d45a70b4154395711647073413e01f370ab
Miles Conn <milesjconn@gmail.com>
2024-02-14 16:34:27 -0500

    Fixed captions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            figcaption: {
+              p: {
+                "text-align": "center",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
