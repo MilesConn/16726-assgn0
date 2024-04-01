0e796ae9c8836f290817629d2d95c734673a0a7f
Miles Conn <milesjconn@gmail.com>
2024-04-01 14:34:02 -0400

    Finished assgn3 writeup

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,9 @@ export default defineConfig({
   output: "static",
   integrations: [tailwind(), mdx()],
   // TODO: specify project
-  site: "https://www.andrew.cmu.edu/course/16-726-sp24/projects/mconn/proj2/",
+  site: "https://www.andrew.cmu.edu/course/16-726-sp24/projects/mconn/proj3/",
   build: {
     assetsPrefix:
-      "https://www.andrew.cmu.edu/course/16-726-sp24/projects/mconn/proj2/",
+      "https://www.andrew.cmu.edu/course/16-726-sp24/projects/mconn/proj3/",
   },
 });
